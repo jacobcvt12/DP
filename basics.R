97c77308b8211e005cd16a782001827710ce49bb
Jacob Carey <jacobcvt12@gmail.com>
2016-06-26 21:39:04 -0400

    Correct syntax, allow function as parameter

--- basics.R
+++ basics.R
@@ -1,13 +1,6 @@
 # make n draws from G ~ DP(alpha, H)
 # where alpha is [0.05, 0.5, 1] and H ~ Normal(0, 1)
-n <- 500
-
-rdp <- function(n, alpha=1, H="normal", k=1000, ...) {
-
-    .dens <- list("normal"=list("r"=rnorm,
-                                "d"=dnorm),
-                  "t"=list("r"=rt,
-                           "d"=dt))
+rdp <- function(n, alpha=1, H=rnorm, k=1000, ...) {
 
     # beta_k ~ Beta(1, alpha)
     beta.k <- rbeta(k, 1, alpha)
@@ -20,10 +13,10 @@ rdp <- function(n, alpha=1, H="normal", k=1000, ...) {
     }
 
     # theta_k^* ~ H
-    theta.star.k <- .dens[[H]][["r"]](k, ...)
+    theta.star.k <- H(k, ...)
 
     # G = sum_1^{infty} pi_k delta_{theta_k^*}
-    G <- samply(theta.star.k, prob=pi.k, replace=TRUE)
+    G <- sample(theta.star.k, prob=pi.k, replace=TRUE)
 
     return(G)
 }
