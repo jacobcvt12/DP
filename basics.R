0910c0e1436a8f74f477d1d0e4d3a1f2c5ddfc39
Jacob Carey <JCarey@telligen.com>
2016-08-03 13:02:24 -0400

    Add CRP for DP

--- basics.R
+++ basics.R
@@ -29,3 +29,41 @@ rdp <- function(n, alpha=1, H=rnorm, k=1000, ...) {
 
     return(G)
 }
+
+#' Take n draws from G ~ DP(alpha, H) using CRP
+#'
+#' @examples
+#'     X1 <- rcrp(k=1000, mean=4, sd=20)
+#'     X2 <- rcrp(k=1000, H=rbeta, shape1=2, shape2=2)
+#' @param alpha precision parameter
+#' @param H baseline distribution
+#' @param k number of customers to seat
+#' @param ... named parameters passed to baseline distribution
+#' @return A vector of draws from DP
+rcrp <- function(alpha=1, H=rnorm, k=1000, ...) {
+
+    # number of customers at each table
+    m <- numeric(k)
+    
+    # assign first customer to first table w/ prob 1
+    m[1] <- 1
+    
+    # now conditionally assign (k-1) customers to tables
+    for (i in 2:k) {
+        # choose already seated table w/ prob prop to customers
+        prob <- m
+        
+        # choose new table with prob proportional to alpha
+        prob[sum(m > 0) + 1] <- alpha
+        
+        m <- m + rmultinom(1, 1, prob)
+    }
+    
+    m <- m[m > 0]
+    m <- m / sum(m)
+    
+    # phi_k ~ H
+    phi.k <- H(length(m), ...)
+    
+    return(cbind(m, phi.k))
+}
